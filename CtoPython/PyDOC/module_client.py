44b86f69f17b4f8cab7349f57af4ca352ba1a9f9
weimo3221 <weichaoren@buaa.edu.cn>
2024-09-09 10:34:27 +0800

    add english explains

--- CtoPython/PyDOC/module_client.py
+++ CtoPython/PyDOC/module_client.py
@@ -14,6 +14,8 @@ def request_data(data):
     # 用于请求数据
     url = 'http://127.0.0.1:5000/'
     # 这里需要根据module_app或者module_app_model里面的设置动态调整，现在调整的是一样的
+    # (Here it needs to be dynamically adjusted according to the settings inside module_app_model.py
+    # , which are now adjusted the same way)
     params = {'input': data}
     response = requests.get(url, params=params)
     return response.text
@@ -21,6 +23,7 @@ def request_data(data):
 
 def run(data):
     # data是16进制的字符串，如"0011223344..."，但保证长度为2的倍数
+    # (data is a hexadecimal string, such as "0011223344...". but guaranteed to be a multiple of 2)
     data = data.strip().replace('\n', '').replace('\r', '')
     result = request_data(data)
     return result
